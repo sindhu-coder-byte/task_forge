3c4a388161777b9b03270752a20c070bb5753890
sindhu-coder-byte <projects_github_email@example.com>
2026-04-21 15:23:47 +0530

    fix JS null error in board page

--- core/apps.py
+++ core/apps.py
@@ -4,15 +4,4 @@ class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
-    def ready(self):
-        import os
-        from django.contrib.auth import get_user_model
-
-        User = get_user_model()
-
-        username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
-
-        if username and password and not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
+    
