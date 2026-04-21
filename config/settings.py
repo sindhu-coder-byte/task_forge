37e855d12f88210c7d76d565253a2f6e612bbd5b
sindhu-coder-byte <projects_github_email@example.com>
2026-04-21 13:24:12 +0530

    clean requirements for render

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7x%c$_i_s1&0@8&%!i%6_=gbm5qvbqrrk#p#s5rvb1yg=hos8'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -213,8 +212,6 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = "sindhujabe94@gmail.com"
-EMAIL_HOST_PASSWORD = "blhd ipbl ziuk gmor"
 
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
